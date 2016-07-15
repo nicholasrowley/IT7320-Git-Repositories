181ce523f42d51f544815f9be31895a65a54e35c
Nick <Nick@Nick-PC>
2016-07-15 17:44:47 +1200

    Added Imports to interface ReservationInterface and added method declaration makeReservation();

--- Assignment1/src/hotelReservation/ReservationInterface.java
+++ Assignment1/src/hotelReservation/ReservationInterface.java
@@ -1,4 +1,10 @@
+package hotelReservation;
+
+import java.rmi.Remote;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface ReservationInterface {
-
+	public ArrayList<String> makeReservation() throws RemoteException;
 }
