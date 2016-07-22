a8d4490f8cff5b61e03ab9d13341f4a1a86ef901
nicholas.rowley <nicholas.rowley@B106-15.soit.local>
2016-07-22 13:26:32 +1200

    edited classpath and ReservationClient class
    Added mysql connector to project and added system outputs in Reservation
    client.

--- Assignment1/src/hotelReservation/ReservationClient.java
+++ Assignment1/src/hotelReservation/ReservationClient.java
@@ -1,9 +1,26 @@
 package hotelReservation;
 
+import java.util.ArrayList;
+
 public class ReservationClient {
+	private ArrayList<String> reservationInput; /* RoomType(VARCHAR max size 25), ArrivalDate(Date), DepartureDate(Date), 
+													OccupantName(VARCHAR max size 50), BookedBy(VARCHAR max size 50)*/
+	//add variable for viewing here. (after add functionality)
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO save reservation input to ArrayList<String> (add error checks later)
+		System.out.println("Enter reservation RoomType(letters max 25): ");
+		//save input code here
+		System.out.println("Enter reservation ArrivalDate(yyyy-mm-dd): ");
+		//save input code here
+		System.out.println("Enter reservation DepartureDate(yyyy-mm-dd): ");
+		//save input code here
+		System.out.println("Enter reservation OccupantName(letters max 50): ");
+		//save input code here
+		System.out.println("Enter reservation BookedBy(letters max 50): ");
+		//save input code here
+		
 
 	}
 
