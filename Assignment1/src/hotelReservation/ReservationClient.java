736f155fd1a969c561cb28c55a3a445cd6c626d5
Nick2 <Nick2@Nick-PC>
2016-07-30 22:03:50 +1200

    added user input requests for the rest of the data fields required.

--- Assignment1/src/hotelReservation/ReservationClient.java
+++ Assignment1/src/hotelReservation/ReservationClient.java
@@ -13,18 +13,30 @@ public class ReservationClient {
 		// TODO save reservation input to ArrayList<String> (add error checks later)
 		System.out.println("Enter reservation RoomType(letters max 25): ");
 		//save input code here
-		Scanner sc = new Scanner(System.in);
-		reservationInput.add(sc.next());
+		Scanner sc1 = new Scanner(System.in);
+		reservationInput.add(sc1.next());
 		System.out.println("Enter reservation ArrivalDate(yyyy-mm-dd): ");
 		//save input code here
+		Scanner sc2 = new Scanner(System.in);
+		reservationInput.add(sc2.next());
 		System.out.println("Enter reservation DepartureDate(yyyy-mm-dd): ");
 		//save input code here
+		Scanner sc3 = new Scanner(System.in);
+		reservationInput.add(sc3.next());
 		System.out.println("Enter reservation OccupantName(letters max 50): ");
 		//save input code here
+		Scanner sc4 = new Scanner(System.in);
+		reservationInput.add(sc4.next());
 		System.out.println("Enter reservation BookedBy(letters max 50): ");
 		//save input code here
+		Scanner sc5 = new Scanner(System.in);
+		reservationInput.add(sc5.next());
 		
-		sc.close();
+		sc1.close();
+		sc2.close();
+		sc3.close();
+		sc4.close();
+		sc5.close();
 		
 
 	}
